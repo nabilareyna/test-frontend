5d5a93f4b26845913489b7f4e078a6553e3c2049
nabilareyna <nabilareynarahmadhani@gmail.com>
2022-02-11 00:27:49 +0700

    first commit

---
+++ src/views/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="sticky top-0 box-border w-full p-4 border-4 text-center bg-gray-200">
+        <h1 style="font-family:'Overpass'" class="font-bold">Fraktur Gaji</h1>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style src="../assets/tailwind.css">
